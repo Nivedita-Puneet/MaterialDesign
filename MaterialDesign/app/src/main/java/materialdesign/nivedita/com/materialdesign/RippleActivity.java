556d15bd4d52b218f1610ac9b338a55a344da534
Nivedita-puneet <nivedita.puneet@gmail.com>
2017-08-10 20:54:05 +1000

    Basics of material design

---
+++ MaterialDesign/app/src/main/java/materialdesign/nivedita/com/materialdesign/RippleActivity.java
@@ -0,0 +1,22 @@
+package materialdesign.nivedita.com.materialdesign;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by PUNEETU on 10-08-2017.
+ */
+
+public class RippleActivity extends Activity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
